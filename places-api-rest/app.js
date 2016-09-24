34cdf2024fad8487144e7798d7d7b7aefd07bfe5
jvicentem <j.vicentem@alumnos.urjc.es>
2016-09-24 02:18:20 +0200

    Created models

--- places-api-rest/app.js
+++ places-api-rest/app.js
@@ -12,6 +12,62 @@ const port = process.env.PORT || 3000
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
+//endpoints
+
+//Get all places
+app.get('/places', (req, res) => {
+    res.status(200).send({places: []})
+})
+
+//Get one place
+app.get('/places/:placeId', (req, res) => {
+    
+})
+
+//Add a place
+app.post('/places', (req, res) => {
+    console.log(req.body)
+    res.status(201).send({})
+})
+
+//Modify one place
+app.put('/places/:placeId', (req, res) => {
+
+})
+
+//Delete one place
+app.delete('/places/:placeID', (req, res) => {
+
+})
+
+//Get all tags
+app.get('/tags', (req, res) => {
+
+})
+
+//Get one tag
+app.get('/tags/:tagName', (req, res) => {
+
+})
+
+//Add a tag
+app.post('/tag', (req, res) => {
+
+})
+
+//Modify one tag
+app.put('/tags/:tagName', (req, res) => {
+
+})
+
+//Delete one tag
+app.delete('/tags/:tagName', (req, res) => {
+
+})
+
+
+
+//Server listening block
 app.listen(port, () => {
     console.log(`Listening on port ${port}!`)
 })
